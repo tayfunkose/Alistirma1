cb37a636b27ae9f2c696b050d664baf5a18bbb4f
tayfunkose <56042707+tayfunkose@users.noreply.github.com>
2019-11-06 15:06:25 +0300

    Add files via upload

---
+++ Alistirma1.8.py
@@ -0,0 +1,8 @@
+
+for i in range(100,1000,2):
+    a=str(i)
+    if int(a[0])==int(a[1]) or int(a[0])==int(a[2]) or int(a[1])==int(a[2]) or int(a[1])==int(a[2])==int(a[3]):
+        print(a,end=" ")
+
+
+
