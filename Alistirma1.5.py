d21a70d028588820d7bf1165488cea337661b43b
tayfunkose <56042707+tayfunkose@users.noreply.github.com>
2019-11-06 14:45:55 +0300

    Add files via upload

---
+++ Alistirma1.5.py
@@ -0,0 +1,58 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: /Users/macbookair/Desktop/Asude C/calismalar.py ==========
+6851
+>>> 
+========== RESTART: /Users/macbookair/Desktop/Asude C/calismalar.py ==========
+999.0
+>>> 
+========== RESTART: /Users/macbookair/Desktop/Asude C/calismalar.py ==========
+77.42092740338364
+>>> 
+========== RESTART: /Users/macbookair/Desktop/Asude C/calismalar.py ==========
+774.5927962484546
+>>> 
+========== RESTART: /Users/macbookair/Desktop/Asude C/calismalar.py ==========
+Traceback (most recent call last):
+  File "/Users/macbookair/Desktop/Asude C/calismalar.py", line 7, in <module>
+    print(sqrt(6*toplam))
+NameError: name 'sqrt' is not defined
+>>> 
+========== RESTART: /Users/macbookair/Desktop/Asude C/calismalar.py ==========
+3.141583104230963
+>>> 
+========= RESTART: /Users/macbookair/Desktop/Asude C/Alistirma1.2.py =========
+Traceback (most recent call last):
+  File "/Users/macbookair/Desktop/Asude C/Alistirma1.2.py", line 9, in <module>
+    print( toplam=toplam+1/faktoriyel)
+TypeError: 'toplam' is an invalid keyword argument for print()
+>>> 
+========= RESTART: /Users/macbookair/Desktop/Asude C/Alistirma1.2.py =========
+1.7182818284590455
+>>> 
+========= RESTART: /Users/macbookair/Desktop/Asude C/Alistirma1.2.py =========
+1.317902151454404
+>>> 
+========= RESTART: /Users/macbookair/Desktop/Asude C/Alistirma1.2.py =========
+1.7182818284590455
+>>> 
+========= RESTART: /Users/macbookair/Desktop/Asude C/Alistirma1.2.py =========
+1.7182818284590455
+>>> 
+========= RESTART: /Users/macbookair/Desktop/Asude C/Alistirma1.2.py =========
+Traceback (most recent call last):
+  File "/Users/macbookair/Desktop/Asude C/Alistirma1.2.py", line 6, in <module>
+    toplam=toplam+(1/faktoriyel)
+ZeroDivisionError: division by zero
+>>> 
+========= RESTART: /Users/macbookair/Desktop/Asude C/Alistirma1.2.py =========
+2.7182818284590455
+>>> 
+========= RESTART: /Users/macbookair/Desktop/Asude C/Alistirma1.3.py =========
+101 111 121 131 141 151 161 171 181 191 202 212 222 232 242 252 262 272 282 292 303 313 323 333 343 353 363 373 383 393 404 414 424 434 444 454 464 474 484 494 505 515 525 535 545 555 565 575 585 595 606 616 626 636 646 656 666 676 686 696 707 717 727 737 747 757 767 777 787 797 808 818 828 838 848 858 868 878 888 898 909 919 929 939 949 959 969 979 989 999 
+>>> 
+========= RESTART: /Users/macbookair/Desktop/Asude C/Alistirma1.4.py =========
+1001 1111 1221 1331 1441 1551 1661 1771 1881 1991 2002 2112 2222 2332 2442 2552 2662 2772 2882 2992 3003 3113 3223 3333 3443 3553 3663 3773 3883 3993 4004 4114 4224 4334 4444 4554 4664 4774 4884 4994 5005 5115 5225 5335 5445 5555 5665 5775 5885 5995 6006 6116 6226 6336 6446 6556 6666 6776 6886 6996 7007 7117 7227 7337 7447 7557 7667 7777 7887 7997 8008 8118 8228 8338 8448 8558 8668 8778 8888 8998 9009 9119 9229 9339 9449 9559 9669 9779 9889 9999 
+>>> 
